a0451f9be7ba14c608a65e0d7a17ea8042681cb7
Rex Cardan <rexcardan@gmail.com>
2020-05-20 12:44:05 -0500

    First working version with constraints

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,19 +1,65 @@
-﻿using Prism.Mvvm;
+﻿using ESAPIX.Interfaces;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Common;
+using VMS.TPS.Common.Model.API;
+using Prism.Commands;
+using System.Windows;
+using F = ESAPIX.Facade.API;
+using ESAPIX.Extensions;
+using ESAPIX.Constraints.DVH;
+using System.Collections.ObjectModel;
 
 namespace Cardan.PlanChecker15.ViewModels
 {
     public class MainViewModel : BindableBase
     {
-        private string _title = "ESAPIX Starter Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+        AppComThread VMS = AppComThread.Instance;
 
         public MainViewModel()
         {
+            EvaluateCommand = new DelegateCommand(() =>
+            {
+                foreach (var pc in Constraints)
+                {
+                    var result = VMS.GetValue(sc =>
+                    {
+                        //Check if we can constrain first
+                        var canConstrain = pc.Constraint.CanConstrain(sc.PlanSetup);
+                        //If not..report why
+                        if (!canConstrain.IsSuccess) { return canConstrain; }
+                        else
+                        {
+                            //Can constrain - so do it
+                            return pc.Constraint.Constrain(sc.PlanSetup);
+                        }
+                    });
+                    //Update UI
+                    pc.Result = result;
+                }
+            });
 
+            CreateConstraints();
         }
+
+        private void CreateConstraints()
+        {
+            Constraints.AddRange(new PlanConstraint[]
+            {
+                new PlanConstraint(ConstraintBuilder.Build("PTV", "Max[%] <= 110")),
+                new PlanConstraint(ConstraintBuilder.Build("Rectum", "V75Gy[%] <= 15")),
+                new PlanConstraint(ConstraintBuilder.Build("Rectum", "V65Gy[%] <= 35")),
+                new PlanConstraint(ConstraintBuilder.Build("Bladder", "V80Gy[%] <= 15")),
+               // new PlanConstraint(new CTDateConstraint())
+            });
+        }
+
+
+        public DelegateCommand EvaluateCommand { get; set; }
+        public ObservableCollection<PlanConstraint> Constraints { get; private set; } = new ObservableCollection<PlanConstraint>();
     }
 }
---
+++ ViewModels/PlanConstraint.cs
@@ -0,0 +1,30 @@
+﻿using ESAPIX.Constraints;
+using Prism.Mvvm;
+
+namespace Cardan.PlanChecker15.ViewModels
+{
+    public class PlanConstraint : BindableBase
+    {
+        public PlanConstraint(IConstraint con)
+        {
+            this.Constraint = con;
+        }
+
+
+        private IConstraint _constraint;
+
+        public IConstraint Constraint
+        {
+            get { return _constraint; }
+            set { SetProperty(ref _constraint, value); }
+        }
+
+        private ConstraintResult _result;
+
+        public ConstraintResult Result
+        {
+            get { return _result; }
+            set { SetProperty(ref _result, value); }
+        }
+    }
+}
