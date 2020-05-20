9a829df6c4bdf3f1268bedac31575b74cb2da919
Rex Cardan <rexcardan@gmail.com>
2020-05-20 12:00:34 -0500

    initial

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Cardan.PlanChecker15.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private string _title = "ESAPIX Starter Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cardan.PlanChecker15.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
